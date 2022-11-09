c60b72e9d6064a28de420145f5b936c8e14b0000
Artaslim <Taslimuddinar@gmail.com>
2022-11-10 04:08:10 +0600

    basic server setup

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+//middle ware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7leckst.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+app.get("/", (req, res) => {
+  res.send("wedding vendor service running on");
+});
+
+app.listen(port, () => {
+  console.log(`wedding vendor service running on ${port}`);
+});
